625d42f97c7e71fe58062290a1c49e08d22b49e5
Mark Ivan Hernandez <m.hernandez@prostrive.io>
2023-11-04 16:02:42 +0800

    Mobile and tablet view of register today section is done

--- src/components/RegisterToday.tsx
+++ src/components/RegisterToday.tsx
@@ -4,7 +4,7 @@ const RegisterToday = () => {
   return (
     <section className="mt-[154.78px] register-today p-[46px] ">
         <ClickableText content="Request Early Access to Get Started" addStyles="text-white"/>
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col lg:flex-row lg:justify-between gap-8 lg:items-center">
             <h1 className="text-[24px] leading-[45px] font-extrabold">Register today & start exploring the endless possibilities</h1>
             <button className="bg-black w-[189.136px] h-[59.398px] rounded-[40px] text-[18px] font-[700] leading-[30px]">Get Started</button>
         </div>
