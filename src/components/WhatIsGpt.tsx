fd7ae27c9a2fb4e6d2b9deaf69ebbdff918566bd
Mark Ivan Hernandez <m.hernandez@prostrive.io>
2023-11-04 15:43:44 +0800

    Mobile and tablet view for what is gpt section done

--- src/components/WhatIsGpt.tsx
+++ src/components/WhatIsGpt.tsx
@@ -25,23 +25,23 @@ const articles:Article[] =[
 
 const WhatIsGpt:React.FC = () => {
   return (
-    <section id="GPT" className="bg-secondary max-w-[1170px] min-h-[695px] px-[73px] py-[74px]">
-      <article className="flex justify-between ">
+    <section id="GPT" className="bg-secondary max-w-[1170px] min-h-[695px] p-10 md:px-[73px] md:py-[74px]">
+      <article className="flex flex-col lg:flex-row justify-between text-justify lg:text-left">
         <TitleWithBars content="What is GPT-3"/>
         <Descriptions content="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." addStyles="max-w-[731px] text-[16px] leading-[30px] font-[500] pr-2 mt-4"/>
       </article>
 
-      <div className="my-[91px] flex justify-between items-center">
-        <GradientTitle content="The possibilities are beyond your imagination" addStyles="text-[34px] max-w-[472px]"/>
+      <div className="my-[50px] lg:my-[91px] flex flex-col gap-12 lg:justify-between lg:items-center lg:flex-row ">
+        <GradientTitle content="The possibilities are beyond your imagination" addStyles="text-xl md:text-[34px] lg:max-w-[472px] leading-[50px]"/>
         <ClickableText content="Explore The Library"/>
       </div>
 
-      <div className="flex">
+      <div className="flex flex-col gap-20 lg:flex-row">
         {
           articles.map(({title, description}, index)=>(
-              <article key={index} className="flex flex-col">
+              <article key={index} className="flex flex-col md:flex-row md:justify-between lg:flex-col">
                 <TitleWithBars content={title}/>
-                <Descriptions content={description}addStyles="max-w-[334px] text-[16px] leading-[30px] font-[500] pr-3 mt-4"/>
+                <Descriptions content={description}addStyles="max-w-[334px] text-[16px] lg:leading-[30px] font-[500] lg:pr-3 mt-4"/>
               </article>
           ))
         }
