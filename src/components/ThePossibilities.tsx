319038531212bdaaf2d307f196bd40b5da8ed14b
Mark Ivan Hernandez <m.hernandez@prostrive.io>
2023-11-04 15:58:57 +0800

    Mobile and tablet view of the possibilites section is done

--- src/components/ThePossibilities.tsx
+++ src/components/ThePossibilities.tsx
@@ -5,12 +5,12 @@ import GradientTitle from "./reusable/GradientTitle"
 
 const ThePossibilities = () => {
   return (
-    <section id="caseStudies" className="mt-[55px] grid grid-cols-2">
+    <section id="caseStudies" className="mt-[55px] grid grid-cols-1 gap-8 md:grid-cols-2">
       <img src={possibility} alt="possibility" />
       <div className="self-end flex flex-col gap-2">
         <ClickableText content="Request Early Access to get Started" addStyles="text-[#71e5ff]"/>
-        <GradientTitle content="The possibilities are beyond your imagination" addStyles="text-[34px] leading-[45px]"/>
-        <Descriptions content="Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of." addStyles="text-[16px] font-[400] leading-[30px]"/>
+        <GradientTitle content="The possibilities are beyond your imagination" addStyles="text-2xl md:text-[28px] lg:text-[34px] leading-[45px]"/>
+        <Descriptions content="Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of." addStyles="text-sm md:text-[14px] lg:text-[16px] font-[400] leading-[30px]"/>
         <ClickableText content="Request Early Access to get Started" addStyles="text-red mt-8"/>
       </div>
     </section>
